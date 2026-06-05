57b94f52d60f527d54a605d6c0450f6be66f1c7f
Pankaj Singh <jaat548986@gmail.com>
2026-06-05 17:38:12 +0530

    Home section code

--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 function Hero() {
     return ( 
     <div className="container p-5">
-        <div className="row text-centre">
+        <div className="row text-center">
             <div className="col">
-                <img src="/Assets/landing.svg" alt="This is Hero image" className='mb-5'/>
-                <h1 className='mt-5'>Invest in everything</h1>
-                <p> Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more. </p>
-                <button className='btn btn-primary'>Sign up for free</button>
+                <img src="/Assets/landing.svg" alt="This is Hero image" className='mb-5 w-75 img-fluid'/>
+                <h1 className='mt-5 fs-3' style={{color:"#424242"}}>Invest in everything</h1>
+                <p style={{color:"#424242"}}className='fs-5'> Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more. </p>
+                <button className='btn btn-primary p-2 fs-5 fw-bold mt-4' style={{width:"20%"}}>Sign up for free</button>
             </div>
         </div>
     </div>
