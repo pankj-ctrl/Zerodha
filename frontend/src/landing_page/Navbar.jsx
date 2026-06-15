0994c6173f592fa68d683f7fd2dfdf433dd644eb
Pankaj Singh <jaat548986@gmail.com>
2026-06-15 13:25:11 +0530

    Navbar

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -1,8 +1,43 @@
 import React from 'react';
 function Navbar() {
-    return ( <>
-    <h1>Navbar</h1>
-    </> );
+    return (
+            <nav class="navbar navbar-expand-lg bg-light border-bottom p-3">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <img src="/public/Assets/logo.svg" alt="logo image"  style={{width:"20%"}}/>
+                        </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        
+                        <form class="d-flex" role="search">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link " aria-current="page" href="#">Signup</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Products</a>
+                            </li>
+                            
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Support</a>
+                            </li>
+
+                        </ul>
+                        </form>
+                        <i class="fa-solid fa-bars" style={{color:"#666",marginLeft:"2rem",fontSize:"1.2rem"}}></i>
+                    </div>
+                </div>
+            </nav>
+    );
 }
 
 export default Navbar;
--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 function OpenAccount() {
     return (
-        <div className="container">
+        <div className="container mt-5 p-4">
             <div className="row text-center">
-                <h3>Open a Zerodha account</h3>
-
-                <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
-                <button></button>
+                <h3 className='mt-5 fs-3 fw-bold' style={{color:"#424242"}}>Open a Zerodha account</h3>
+                <p style={{color:"#424242"}}className='fs-5'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+                <button className='btn btn-primary p-2 fs-5 mt-4 mb-5' style={{width:"20%", margin:"0 auto"}}>Sign Up for free</button>
             </div>
         </div>
     );
