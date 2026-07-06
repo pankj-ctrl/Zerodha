fb177ae198eaa4c7284828290c7b11ef8815cd75
Pankaj Singh <jaat548986@gmail.com>
2026-07-06 17:06:50 +0530

    some change in image url

--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -7,17 +7,17 @@ function Hero() {
             </div>
             <div className="row text-center" style={{marginTop:"5rem"}}>
                 <div className="col">
-                    <img src="/public/Assets/pricing0.svg" alt="This is pricing 0 svg image" style={{width:"75%"}} />
+                    <img src="/Assets/pricing0.svg" alt="This is pricing 0 svg image" style={{width:"75%"}} />
                     <h4 className="heading4" >Free equity delivery</h4>
                     <p className="pricingPara"> All equity delivery investments (NSE, BSE), <br /> are absolutely free — ₹ 0 brokerage. </p>
                 </div>
                 <div className="col">
-                    <img src="/public/Assets/other-trades.svg" alt="This is pricing 20 svg image" style={{width:"75%"}}/>
+                    <img src="/Assets/other-trades.svg" alt="This is pricing 20 svg image" style={{width:"75%"}}/>
                     <h4  className="heading4">Intraday and F&O trades</h4>
                     <p className="pricingPara">Flat ₹ 20 or 0.03% (whichever is lower) per <br /> executed order on intraday trades across <br /> equity, currency, and commodity trades. <br /> Flat ₹20 on all option trades. </p>
                 </div>
                 <div className="col">
-                    <img src="/public/Assets/pricing0.svg" alt="This is also pricing 0 svg image" style={{width:"75%"}}/>
+                    <img src="/Assets/pricing0.svg" alt="This is also pricing 0 svg image" style={{width:"75%"}}/>
                     <h4 className="heading4">Free direct MF</h4>
                     <p className="pricingPara">All direct mutual fund investments are <br />absolutely free — ₹ 0 commissions & DP <br /> charges. </p>
                 </div>
